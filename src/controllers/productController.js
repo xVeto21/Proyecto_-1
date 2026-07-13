7264c8799c1e47d2b93242e4fd6147fa66701970
Veto <st4120@utr.edu.mx>
2026-07-13 12:16:12 -0600

    Terminado

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -53,3 +53,51 @@ exports.createProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+// PUT actualizar producto
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Producto no encontrado"
+      });
+    }
+
+    res.json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+// DELETE eliminar producto
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Producto no encontrado"
+      });
+    }
+
+    res.json({
+      message: "Producto eliminado correctamente"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,11 +4,19 @@ const router = express.Router();
 const {
   getProducts,
   getProductById,
-  createProduct
+  createProduct,
+  updateProduct,
+  deleteProduct
 } = require("../controllers/productController");
 
 router.get("/", getProducts);
+
 router.get("/:id", getProductById);
+
 router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
